9fc2293e949050b04a29b191dd8a82be54ced80e
Ricardo <ricolvs123@gmail.com>
2019-08-26 19:47:02 -0300

    clear_cpu, font data

---
+++ include/fontset.h
@@ -0,0 +1,8 @@
+#ifndef FONTSET_H_INCLUDED
+#define FONTSET_H_INCLUDED
+
+#include <stdint.h>
+
+extern const uint8_t fontset[16][5];
+
+#endif // FONTSET_H_INCLUDED
--- src/cpu.c
+++ src/cpu.c
@@ -1,8 +1,20 @@
 #include "cpu.h"
 #include "cpu_info.h"
+#include "fontset.h"
+#include <string.h>
 
 typedef struct CPU_info CPU_info;
 
+static void load_fonts(CPU_info * cpu){
+    for(int i=0;i<16;i++){
+        for(int j=0;j<5;j++){
+            cpu->RAM[i*5+j]=fontset[i][j];
+        }
+    }
+}
+
 void clear_cpu(CPU_info * cpu){
-    //TODO
+    memset(cpu,0,sizeof(CPU_info));
+    load_fonts(cpu);
+    cpu->PC=0x200;
 }
